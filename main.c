639837176c889b49f83b95802db14991e27c4b31
Diogo Carvalho <diogocarvalho.bash@gmail.com>
2022-12-01 22:22:44 +0000

    01122022

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include "pintor.c"
 
-const unsigned int WIDTH = 2000;
-const unsigned int HEIGHT = 2000;
+const unsigned int WIDTH = 16;
+const unsigned int HEIGHT = 16;
 
 int main(int argc, char *argv[]) {
     struct Painting p = newPainting("painting1.ppm", WIDTH, HEIGHT);
+    paintPixel(1, 1, &p, WHITE);
+    paintPixel(5, 5, &p, WHITE);
+    paintPixel(2, 7, &p, WHITE);
+
     if(exportPainting(&p) == 0) {
         printf("%s", "error creating painting...");
         return 1;
--- pintor.c
+++ pintor.c
@@ -8,7 +8,11 @@ struct RGB {
 };
 
 // COLOR CONSTANTS
-const struct RGB WHITE = {255, 255, 255};
+static unsigned char WHITE[3] = {255, 255, 255};
+static unsigned char BLACK[3] = {0, 0, 0};
+static unsigned char RED[3] = {255, 0, 0};
+static unsigned char GREEN[3] = {0, 255, 0};
+static unsigned char BLUE[3] = {0, 0, 255};
 
 void printRGB(struct RGB* rgb) {
     printf("r: %d, g: %d, b: %d", rgb->r, rgb->g, rgb->b);
@@ -18,7 +22,7 @@ struct Painting {
     char* name;
     size_t width;
     size_t height;
-    struct RGB* map;
+    unsigned char** map;
 };
 
 struct Painting newPainting(char* name, size_t width, size_t height) {
@@ -26,11 +30,13 @@ struct Painting newPainting(char* name, size_t width, size_t height) {
     p.name = name;
     p.width = width;
     p.height = height;
-    void* s = malloc(width*height*sizeof(struct RGB));
+    void* s = malloc(width*height*sizeof(unsigned char[3]));
     p.map = s;
 
     for(size_t i = 0; i < height*width; i++) {
-            p.map[i] = WHITE;
+            p.map[i][0] = BLACK[0];
+            p.map[i][1] = BLACK[1];
+            p.map[i][2] = BLACK[2];
     }
 
     free(s);
@@ -43,14 +49,29 @@ int exportPainting(struct Painting* painting) {
         fclose(f);
         return 0;
     }
-    fprintf(f, "P6\n%zu %zu 255\n", painting->width, painting->height);
+    fprintf(f, "P6\n%d %d 255\n", (int) painting->width, (int) painting->height);
+    printf("PAINTING SIZE: %d %d\n", (int) painting->width, (int) painting->height);
 
     for(size_t i = 0; i < painting->height*painting->width; i++) {
         // char rgb[100] = "";
         // sprintf(rgb,"%d %d %d", painting->map[i].r, painting->map[i].g, painting->map[i].b);
-        fputs("255 255 255\n", f);
+        fwrite(painting->map[painting->height*painting->width], 1, 3, f);
     }
 
     fclose(f);
+    return 1;
+}
+
+int paintPixel(int x, int y, struct Painting* p, unsigned char color[3]) {
+    // returns 0 if the point given is out of reach
+    if(x*y > p->height*p->width) return 0;
+    p->map[x*y] = color;
+    return 1;
+}
+
+int paintSquareFill(int x, int y, int size, struct Painting* p, struct RGB color) {
+    for(int i = x; i < size; i++) {
+        //
+    }
     return 1;
 }
